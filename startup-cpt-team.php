34e4cc243796dd00d9fe9c6bf3eec51f27da5064
yozz <yozz@Manava.local>
2015-08-18 21:54:05 -0400

    GitHub Plugin Updater

--- startup-cpt-team.php
+++ startup-cpt-team.php
@@ -1,10 +1,34 @@
 <?php
 /*
-Plugin Name: StartUp Team Custom Post Type
+Plugin Name: StartUp Team
 Description: Le plugin pour activer le Custom Post Team
 Author: Yann Caplain
-Version: 1.1.0
+Version: 1.2.0
 */
+
+//GitHub Plugin Updater
+function startup_reloaded_team_updater() {
+	include_once 'lib/updater.php';
+	define( 'WP_GITHUB_FORCE_UPDATE', true );
+	if ( is_admin() ) {
+		$config = array(
+			'slug' => plugin_basename( __FILE__ ),
+			'proper_folder_name' => 'startup-cpt-team',
+			'api_url' => 'https://api.github.com/repos/yozzi/startup-cpt-team',
+			'raw_url' => 'https://raw.github.com/yozzi/startup-cpt-team/master',
+			'github_url' => 'https://github.com/yozzi/startup-cpt-team',
+			'zip_url' => 'https://github.com/yozzi/startup-cpt-team/archive/master.zip',
+			'sslverify' => true,
+			'requires' => '3.0',
+			'tested' => '3.3',
+			'readme' => 'README.md',
+			'access_token' => '',
+		);
+		new WP_GitHub_Updater( $config );
+	}
+}
+
+add_action( 'init', 'startup_reloaded_team_updater' );
 
 //CPT
 function startup_reloaded_team() {
