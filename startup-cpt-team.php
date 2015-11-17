110c409c67211c97e63f77b5b1304676f67f5ab3
yozz <yozz.online@gmail.com>
2015-11-17 15:23:05 -0500

    Internationalization

--- startup-cpt-team.php
+++ startup-cpt-team.php
@@ -34,24 +34,24 @@ add_action( 'init', 'startup_reloaded_team_updater' );
 //CPT
 function startup_reloaded_team() {
 	$labels = array(
-		'name'                => _x( 'Team members', 'Post Type General Name', 'text_domain' ),
-		'singular_name'       => _x( 'Team member', 'Post Type Singular Name', 'text_domain' ),
-		'menu_name'           => __( 'Team', 'text_domain' ),
-		'name_admin_bar'      => __( 'Team', 'text_domain' ),
-		'parent_item_colon'   => __( 'Parent Item:', 'text_domain' ),
-		'all_items'           => __( 'All Items', 'text_domain' ),
-		'add_new_item'        => __( 'Add New Item', 'text_domain' ),
-		'add_new'             => __( 'Add New', 'text_domain' ),
-		'new_item'            => __( 'New Item', 'text_domain' ),
-		'edit_item'           => __( 'Edit Item', 'text_domain' ),
-		'update_item'         => __( 'Update Item', 'text_domain' ),
-		'view_item'           => __( 'View Item', 'text_domain' ),
-		'search_items'        => __( 'Search Item', 'text_domain' ),
-		'not_found'           => __( 'Not found', 'text_domain' ),
-		'not_found_in_trash'  => __( 'Not found in Trash', 'text_domain' )
+		'name'                => _x( 'Team members', 'Post Type General Name', 'startup-cpt-team' ),
+		'singular_name'       => _x( 'Team member', 'Post Type Singular Name', 'startup-cpt-team' ),
+		'menu_name'           => __( 'Team', 'startup-cpt-team' ),
+		'name_admin_bar'      => __( 'Team', 'startup-cpt-team' ),
+		'parent_item_colon'   => __( 'Parent Item:', 'startup-cpt-team' ),
+		'all_items'           => __( 'All Items', 'startup-cpt-team' ),
+		'add_new_item'        => __( 'Add New Item', 'startup-cpt-team' ),
+		'add_new'             => __( 'Add New', 'startup-cpt-team' ),
+		'new_item'            => __( 'New Item', 'startup-cpt-team' ),
+		'edit_item'           => __( 'Edit Item', 'startup-cpt-team' ),
+		'update_item'         => __( 'Update Item', 'startup-cpt-team' ),
+		'view_item'           => __( 'View Item', 'startup-cpt-team' ),
+		'search_items'        => __( 'Search Item', 'startup-cpt-team' ),
+		'not_found'           => __( 'Not found', 'startup-cpt-team' ),
+		'not_found_in_trash'  => __( 'Not found in Trash', 'startup-cpt-team' )
 	);
 	$args = array(
-		'label'               => __( 'team', 'text_domain' ),
+		'label'               => __( 'team', 'startup-cpt-team' ),
 		'labels'              => $labels,
 		'supports'            => array( 'title', 'editor', 'thumbnail', 'revisions' ),
 		'hierarchical'        => true,
