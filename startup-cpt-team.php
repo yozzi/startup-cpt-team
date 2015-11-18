236f82cacaedc778531441959d608c932ed7c89e
yozz <yozz.online@gmail.com>
2015-11-18 14:50:14 -0500

    Disabled Github Updater

--- startup-cpt-team.php
+++ startup-cpt-team.php
@@ -29,7 +29,7 @@ function startup_reloaded_team_updater() {
 	}
 }
 
-add_action( 'init', 'startup_reloaded_team_updater' );
+//add_action( 'init', 'startup_reloaded_team_updater' );
 
 //CPT
 function startup_reloaded_team() {
